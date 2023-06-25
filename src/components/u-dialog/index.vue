1605b17e02f983cc91a5d84c40894015b7c35b35
xnorain001 <3385842328@qq.com>
2023-06-25 19:49:56 +0800

    feat: dialog component

---
+++ src/components/u-dialog/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="modelValue" class="u-dialog">
+    <slot name="header"></slot>
+    <slot name="main"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { toRefs } from 'vue'
+
+const props = defineProps<{ modelValue: boolean }>()
+const { modelValue } = toRefs(props)
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import UTab from './components/u-tab/index.vue'
 import UTabs from './components/u-tabs/index.vue'
 import UInput from './components/u-input/index.vue'
+import UDialog from './components/u-dialog/index.vue'
 import UTabPannel from './components/u-tab-panel/index.vue'
 import UTabPannels from './components/u-tab-panels/index.vue'
 
@@ -10,6 +11,7 @@ const undefinedElement = {
       .component('UTab', UTab)
       .component('UTabs', UTabs)
       .component('UInput', UInput)
+      .component('UDialog', UDialog)
       .component('UTabPannel', UTabPannel)
       .component('UTabPannels', UTabPannels)
   }
