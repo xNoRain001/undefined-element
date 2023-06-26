df66faa743dc158522833a2e1445b6ec5b487bbf
xnorain001 <3385842328@qq.com>
2023-06-26 21:26:28 +0800

    update: get slider width

--- src/components/u-slider-innter/index.vue
+++ src/components/u-slider-innter/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    ref="inner" 
+    ref="sliderInner" 
     class="u-slider-inner"
     @mousedown="mousedownHandler($event)"
     @mousemove="mousemoveHandler($event)"
@@ -11,24 +11,57 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, toRefs } from 'vue'
+import { onMounted, ref, toRefs } from 'vue'
 
-const props = defineProps<{ modelValue: number }>()
+const props = defineProps<{ 
+  modelValue: number,
+  min?: number,
+  max?: number
+}>()
 const emit = defineEmits<{ 'update:modelValue': [value: number] }>()
-const { modelValue } = toRefs(props)
+const { min, max, modelValue } = toRefs(props)
+const _max = ref(0)
 
 let prevOffset = 0
 let dragging = false
-const inner = ref<HTMLElement | null>(null)
+const slider = ref<HTMLElement | null>(null)
+const sliderInner = ref<HTMLElement | null>(null)
+const sliderWidth = ref(0)
+
+const initMax = () => {
+  sliderWidth.value = getSliderWidth()
+  _max.value = max?.value || sliderWidth.value
+}
+
+const initOffset = () => {
+  // const offset = Math.floor(Number((modelValue.value / _max.value)) * sliderWidth.value)
+  // sliderInner.value!.style.left = `${ offset }px`
+  // console.log((offset / sliderWidth.value) * _max.value)
+}
+
+const getSliderWidth = () => getSlider().clientWidth
+
+const getSlider = () => {
+  let slider = sliderInner.value as HTMLElement
+
+  while (!slider.classList.contains('u-slider')) {
+    slider = slider.parentNode as HTMLElement
+  }
+
+  return slider
+}
 
 const updateDragState = () => dragging = !dragging
 
 const updateOffset = (e: MouseEvent) => {
   const { clientX, clientY } = e
+  const offset = modelValue.value + (clientX - prevOffset)
   
-  emit('update:modelValue', modelValue.value + (clientX - prevOffset))
-  prevOffset = clientX
-  inner.value!.style.left = `${ modelValue.value }px`
+  if (offset > 0 && offset < sliderWidth.value) {
+    emit('update:modelValue', offset)
+    prevOffset = clientX
+    sliderInner.value!.style.left = `${ offset }px`
+  }
 }
 
 const addGlobalEventListener = () => {
@@ -63,6 +96,11 @@ const mouseupHandler = () => {
   updateDragState()
   removeGlobalEventListener()
 }
+
+onMounted(() => {
+  initMax()
+  initOffset()
+})
 </script>
 
 <style scoped>
