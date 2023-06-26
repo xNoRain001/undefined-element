0af3e40756b316e30a0335a70b52a023e7b7008f
xNoRain001 <3385842328@qq.com>
2023-06-26 23:48:13 +0800

    feat: splitter component

---
+++ src/components/u-splitter/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="u-splitter">
+    <div class="before">
+      <slot name="before"></slot>
+    </div>
+    <div ref="splitter">
+      <slot name="splitter"></slot>
+    </div>
+    <div class="after">
+      <slot name="after"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+const splitter = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+
+})
+</script>
+
+<style scoped>
+.u-splitter {
+  display: flex;
+  justify-content: space-between;
+}
+
+.before,
+.after {
+  flex-grow: 1;
+}
+</style>
