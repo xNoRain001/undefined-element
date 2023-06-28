851eae3c986613a43b8066cf3b65cc240fcb32ef
xnorain001 <3385842328@qq.com>
2023-06-28 21:30:25 +0800

    update: dialog component

--- src/components/u-dialog/index.vue
+++ src/components/u-dialog/index.vue
@@ -1,14 +1,52 @@
 <template>
-  <div v-if="modelValue" class="u-dialog">
-    <slot name="header"></slot>
-    <slot name="main"></slot>
-    <slot name="footer"></slot>
-  </div>
+  <Teleport to="body">
+    <div v-if="modelValue" class="u-dialog">
+      <div 
+        class="u-dialog-backdrop" 
+        :style="{ 
+          backgroundColor: modelValue ? 'rgba(0, 0, 0, .4)' : 'transparent' 
+        }" 
+      >
+      </div>
+      <div class="u-dialog-inner">
+        <div>
+          <slot name="default"></slot>
+        </div>
+      </div>
+    </div>
+  </Teleport>
 </template>
 
 <script lang="ts" setup>
-import { toRefs } from 'vue'
+import { toRefs, computed } from 'vue'
 
-const props = defineProps<{ modelValue: boolean }>()
-const { modelValue } = toRefs(props)
+const props = withDefaults(defineProps<{ 
+  modelValue: boolean,
+  position?: 'top' | 'right' | 'bottom' | 'left' | 'center'
+}>(), { position: 'center' })
+const { modelValue, position } = toRefs(props)
 </script>
+
+<style scoped>
+.u-dialog {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+}
+.u-dialog-backdrop {
+  width: 100%;
+  height: 100%;
+  transition: background-color .3s;
+}
+
+.u-dialog-inner{
+  width: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  top: 0;
+}
+</style>
