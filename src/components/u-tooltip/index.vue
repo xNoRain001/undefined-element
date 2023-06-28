77a40a2d0dbbb1e4e6bf2edc0179d88a83c8cc7c
xnorain001 <3385842328@qq.com>
2023-06-28 19:43:36 +0800

    update: add position option

--- src/components/u-tooltip/index.vue
+++ src/components/u-tooltip/index.vue
@@ -9,35 +9,60 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { ref, toRefs, onMounted } from 'vue'
 
+const props = withDefaults(defineProps<{ 
+  position?: 'top' | 'right' | 'bottom' | 'left'
+}>(), {
+  position: 'bottom'
+})
+const { position } = toRefs(props)
 const tooltip = ref<HTMLElement | null>(null)
 const tooltipContainer = ref<HTMLElement | null>(null)
+console.log(position.value)
 
 const displayTooltip = (
   target: HTMLElement,
-  width: number
+  width: number,
+  height: number
 ) => {
-  const { bottom, left, right } = target.getBoundingClientRect()
-  const _left = left + (right - left) / 2 - width / 2
+  const _position = position.value
+  const { top, right, bottom, left } = target.getBoundingClientRect()
+  const _left = _position === 'left' 
+    ? left - width 
+    : _position === 'right'
+      ? right
+      : left + (right - left) / 2 - width / 2 // center
+  const _top = _position === 'top' 
+    ? top - height 
+    : /^left|right$/.test(_position)
+      ? top
+      : bottom
 
   tooltip.value!.style.cssText = `
     opacity: 1;
-    top: ${ bottom }px;
+    top: ${ _top }px;
     left: ${ _left }px;
   `
 }
 
 const hideTooltip = () => {
   tooltip.value!.style.opacity = '0'
+
+  setTimeout(() => {
+    tooltip.value!.style.cssText = `
+      top: 0;
+      left: 0;
+    `
+  }, 300)
 }
 
 const addEventListeners = (target: HTMLElement) => {
   // TODO: dynamic content
-  const width = tooltip.value!.clientWidth
-
+  const { clientWidth, clientHeight } = tooltip.value as HTMLElement
+  
   target.addEventListener('mouseenter', () => {
-    displayTooltip(target, width)
+    displayTooltip(target, clientWidth, clientHeight)
   })
 
   target.addEventListener('mouseleave', () => {
