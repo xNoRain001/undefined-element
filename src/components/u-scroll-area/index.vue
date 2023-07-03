e4285f347d923103f3f0e958f7eb0c1497aaf129
xnorain001 <3385842328@qq.com>
2023-07-03 21:25:39 +0800

    update: scroll area component

--- src/components/u-scroll-area/index.vue
+++ src/components/u-scroll-area/index.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="u-scroll-area" :style="{ height: `${ height }px` }">
+  <div 
+    class="u-scroll-area" 
+    :style="{
+      width,
+      height
+    }"
+    @mousedown="mousedownHandler"
+    @mousemove="mousemoveHandler"
+    @mouseup="mouseupHandler"
+  >
     <div 
       @scroll="scrollHandler" 
       ref="container" 
       class="u-scroll-area-container"
     >
-      <div ref="content" class="u-scroll-area-content">
+      <div class="u-scroll-area-content">
         <slot></slot>
       </div>
     </div>
-    <div class="u-scroll-area-bar">
+    <div class="u-scroll-area-bar-y" ref="barY">
       <div 
-        ref="thumb" 
-        class="u-scroll-area-thumb" 
+        ref="thumbY" 
+        class="u-scroll-area-thumb-y" 
         :style="{ height: `${ thumbHeight }px` }"
+      ></div>
+    </div>
+    <div class="u-scroll-area-bar-x" ref="barX">
+      <div 
+        ref="thumbX" 
+        class="u-scroll-area-thumb-x" 
+        :style="{ width: `${ thumbWidth }px` }"
       ></div>
     </div>
   </div>
@@ -22,29 +38,88 @@
 <script lang="ts" setup>
 import { ref, toRefs, onMounted } from 'vue'
 
-const props = defineProps<{ height?: number | string }>()
-const { height } = toRefs(props)
-const thumb = ref<HTMLElement | null>(null)
-const content = ref<HTMLElement | null>(null)
+const props = withDefaults(defineProps<{ 
+  width?: string,
+  height?: string
+}>(), {
+  width: '100%',
+  height: '100%'
+})
+const { width, height } = toRefs(props)
+const barX = ref<HTMLElement | null>(null)
+const barY = ref<HTMLElement | null>(null)
+const thumbX = ref<HTMLElement | null>(null)
+const thumbY = ref<HTMLLIElement | null>(null)
 const container = ref<HTMLElement | null>(null)
 const thumbHeight = ref(0)
+const thumbWidth = ref(0)
 
 const initThumbSize = () => {
-  if (!height?.value || content.value!.clientHeight < Number(height.value)) {
-    return
+  const { 
+    scrollWidth,
+    clientWidth,
+    scrollHeight, 
+    clientHeight
+  } = container.value as HTMLElement
+
+  if (scrollHeight > clientHeight) {
+    thumbHeight.value = clientHeight * (clientHeight / scrollHeight)
   }
 
-  const { scrollHeight, clientHeight } = container.value as HTMLElement
-  thumbHeight.value = clientHeight * (clientHeight / scrollHeight)
+  if (scrollWidth > clientWidth) {
+    thumbWidth.value = clientWidth * (clientWidth / scrollWidth)
+  }
 }
 
 const scrollHandler = (e: Event) => {
-  const { scrollTop, scrollHeight, clientHeight } = e.currentTarget as HTMLElement
+  const { 
+    scrollTop,
+    scrollLeft,
+    scrollHeight, 
+    clientHeight,
+    scrollWidth,
+    clientWidth
+  } = e.currentTarget as HTMLElement
   const scrollAreaHeight = scrollHeight - clientHeight
+  const scrollAreaWidth = scrollWidth - clientWidth
+  const thumnScrollableWidth = clientWidth - thumbWidth.value
   const thumnScrollableHeight = clientHeight - thumbHeight.value
-  const offset = Number((scrollTop / scrollAreaHeight * thumnScrollableHeight).toFixed(2))
+  const offsetX = Number((scrollLeft / scrollAreaWidth * thumnScrollableWidth).toFixed(2))
+  const offsetY = Number((scrollTop / scrollAreaHeight * thumnScrollableHeight).toFixed(2))
 
-  thumb.value!.style.top = `${ offset }px`
+  thumbX.value!.style.left = `${ offsetX }px`
+  thumbY.value!.style.top = `${ offsetY }px`
+}
+
+let dragging = false
+let prevOffset = 0
+
+const mousedownHandler = (e: MouseEvent) => {
+  const {target} = e
+
+  if (target === thumbX.value || target === thumbY.value) {
+    dragging = true
+    prevOffset = e.clientX
+  }
+
+  if (target === barX.value || target === barY.value) {
+  }
+}
+
+const mousemoveHandler = (e: MouseEvent) => {
+  if (!dragging) {
+    return
+  }
+
+  const { clientX } = e
+  const offset = clientX - prevOffset
+
+  thumbX.value!.style.left = `${ parseInt(thumbX.value!.style.left) || 0 + offset }px`
+  prevOffset = clientX
+}
+
+const mouseupHandler = () => {
+  dragging = false
 }
 
 onMounted(() => {
@@ -54,43 +129,60 @@ onMounted(() => {
 
 <style>
 .u-scroll-area {
-  width: 100%;
   position: relative;
 }
 
 .u-scroll-area-container {
+  width: 100%;
   height: 100%;
-  overflow-y: auto;
+  overflow: auto;
 }
 
 .u-scroll-area-container::-webkit-scrollbar {
   display: none;
 }
 
-.u-scroll-area-bar {
+.u-scroll-area-bar-x,
+.u-scroll-area-bar-y {
   position: absolute;
-  top: 0;
   right: 0;
   bottom: 0;
+}
+
+.u-scroll-area-bar-x {
+  left: 0;
+  height: 6px;
+}
+.u-scroll-area-bar-y {
+  top: 0;
   width: 6px;
 }
 
-.u-scroll-area-thumb {
-  width: 100%;
+.u-scroll-area-thumb-x,
+.u-scroll-area-thumb-y {
   background-color: rgba(144, 147, 153, .3);
   border-radius: 4px;
   display: none;
   position: absolute;
-  right: 0;
   cursor: pointer;
   transition: background-color .3s;
 }
 
-.u-scroll-area-thumb:hover {
+.u-scroll-area-thumb-x {
+  height: 100%;
+}
+
+.u-scroll-area-thumb-y {
+  width: 100%;
+}
+
+.u-scroll-area-thumb-x:hover,
+.u-scroll-area-thumb-y:hover {
   background-color: rgba(144, 147, 153, .5);
 }
 
-.u-scroll-area:hover .u-scroll-area-thumb {
+.u-scroll-area:hover .u-scroll-area-thumb-x,
+.u-scroll-area:hover .u-scroll-area-thumb-y {
   display: block;
 }
 </style>
