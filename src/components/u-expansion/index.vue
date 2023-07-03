f35b4cf120df349c82735e0a187f01a17df837f9
xnorain001 <3385842328@qq.com>
2023-07-03 15:03:12 +0800

    update: expansion component

--- src/components/u-expansion/index.vue
+++ src/components/u-expansion/index.vue
@@ -1,39 +1,42 @@
 <template>
   <div class="u-expansion">
-    <div @click="clickHandler">
-      <slot name="default"></slot>
+    <div @click="clickHandler" class="u-expansion-header">
+      <slot name="header"></slot>
     </div>
-    <div ref="expansionPannel" class="u-expansion-panel">
-      <slot name="panel"></slot>
+    <div 
+      ref="expansionContent" 
+      :style="{ 
+        height: modelValue 
+          ? `${ expansionContent?.scrollHeight }px` || '100%' 
+          : '0' 
+      }" 
+      class="u-expansion-content"
+    >
+      <slot name="content"></slot>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, toRefs, watch } from 'vue'
+import { ref, toRefs } from 'vue'
 
 const props = defineProps<{ modelValue: boolean }>()
 const emit = defineEmits<{ 'update:modelValue': [value: boolean] }>()
 const { modelValue } = toRefs(props)
-const expansionPannel = ref<HTMLElement | null>(null)
+const expansionContent = ref<HTMLElement | null>(null)
 
 const clickHandler = () => {
   emit('update:modelValue', !modelValue.value)
 }
-
-watch(modelValue, newValue => {
-  const _expansionPannel = expansionPannel.value as HTMLElement
-
-  _expansionPannel.style.height = newValue 
-    ? `${ _expansionPannel.scrollHeight }px` 
-    : '0'
-})
 </script>
 
 <style scoped>
-.u-expansion-panel {
-  height: 0;
-  transition: height .3s;
+.u-expansion-header {
+  cursor: pointer;
+}
+
+.u-expansion-content {
+  transition: height var(--u-transition-duration);
   overflow-y: hidden;
 }
 </style>
