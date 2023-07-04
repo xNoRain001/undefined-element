b1a2ee6cbdd48f66f64e855002cdcfb0a656a912
xnorain001 <3385842328@qq.com>
2023-07-04 17:53:39 +0800

    update: dialog component

--- src/components/u-dialog/index.vue
+++ src/components/u-dialog/index.vue
@@ -1,22 +1,20 @@
 <template>
   <Teleport to="body">
-    <div v-if="modelValue" class="u-dialog" @click="closeDialog">
+    <div v-if="_modelValue" class="u-dialog" @click="closeDialog">
       <div 
         class="u-dialog-backdrop" 
         :style="backgroundColorStyle" 
       >
       </div>
-      <div ref="inner" class="u-dialog-inner" :style="postionStyle">
-        <div>
-          <slot name="default"></slot>
-        </div>
+      <div ref="inner" class="u-dialog-inner" :style="positionStyle">
+        <slot name="default"></slot>
       </div>
     </div>
   </Teleport>
 </template>
 
 <script lang="ts" setup>
-import { ref, toRefs, computed } from 'vue'
+import { ref, watch, toRefs, computed } from 'vue'
 
 const props = withDefaults(defineProps<{ 
   modelValue: boolean,
@@ -29,6 +27,7 @@ const props = withDefaults(defineProps<{
 const emit = defineEmits<{ 'update:modelValue': [value: boolean] }>()
 const { modelValue, position, persistent } = toRefs(props)
 const inner = ref<HTMLElement | null>(null)
+const _modelValue = ref(modelValue.value)
 
 const closeDialog = (e: Event) => {
   const _inner = inner.value as HTMLElement
@@ -60,15 +59,13 @@ const closeDialog = (e: Event) => {
 const positionStrategies = {
   top () {
     return {
-      top: 0,
       left: '50%',
-      transform: 'translateX(-50%)'
+      transform: 'translateX(-50%)',
     }
   },
 
   right () {
     return {
-      right: 0,
       top: '50%',
       transform: 'translateY(-50%)'
     }
@@ -76,7 +73,6 @@ const positionStrategies = {
 
   bottom () {
     return {
-      bottom: 0,
       left: '50%',
       transform: 'translateX(-50%)'
     }
@@ -84,7 +80,6 @@ const positionStrategies = {
 
   left () {
     return {
-      left: 0,
       top: '50%',
       transform: 'translateY(-50%)'
     }
@@ -99,11 +94,26 @@ const positionStrategies = {
   }
 }
 
-const postionStyle = computed(() => positionStrategies[position.value]())
+const positionStyle = computed(() => {
+  const _position = position.value
 
-const backgroundColorStyle = computed(() => {
   return {
-    backgroundColor: modelValue.value ? 'rgba(0, 0, 0, .4)' : 'transparent' 
+    ...positionStrategies[_position](),
+    [_position]: modelValue.value ? 0 : '-100%',
+  }
+})
+
+const backgroundColorStyle = computed(() => ({
+  backgroundColor: modelValue.value ? 'rgba(0, 0, 0, .4)' : 'transparent' 
+}))
+
+watch(modelValue, v => {
+  if (v) {
+    _modelValue.value = v
+  } else {
+    setTimeout(() => {
+      _modelValue.value = v
+    }, 300)
   }
 })
 </script>
@@ -117,13 +127,15 @@ const backgroundColorStyle = computed(() => {
   left: 0;
   z-index: 99;
 }
+
 .u-dialog-backdrop {
   width: 100%;
   height: 100%;
-  transition: background-color .3s;
+  transition: background-color var(--u-transition-duration);
 }
 
 .u-dialog-inner {
   position: fixed;
+  transition: all var(--u-transition-duration);
 }
 </style>
