a1c8731bc125ca8fdc18a98b7115bfa8a3502018
xnorain001 <3385842328@qq.com>
2023-07-07 13:51:20 +0800

    update: icon component add type option

--- src/components/u-icon/index.vue
+++ src/components/u-icon/index.vue
@@ -7,7 +7,7 @@
     :fill="color"
   >
     <path 
-      v-for="{ d, style, transform } in segments" 
+      v-for="{ d, style, transform } in paths" 
       :d="d" 
       :style="style" 
       :transform="transform"
@@ -16,33 +16,73 @@
 </template>
 
 <script lang="ts" setup>
-import { toRefs } from 'vue'
+import { computed, toRefs } from 'vue'
 
-import * as icons from '../../assets/icons'
+import icons from '../../assets/icons'
 
 const props = withDefaults(defineProps<{ 
   name: string,
+  type?: 'outlined' | 'filled' | 'rounded' | 'sharp'
   color?: string,
   width?: string | number,
   height?: string | number,
 }>(), {
+  type: 'filled',
   color: '#000',
   width: 24,
   height: 24,
 })
 
-const { name } = toRefs(props)
-const _name = name.value
-const def = icons[`mat${ _name[0].toUpperCase() }${ _name.slice(1) }`]
-const segments: { 
+const { name, type } = toRefs(props)
+const prefixesMap = {
+  filled: 'mat',
+  sharp: 'sharp',
+  rounded: 'round',
+  outlined: 'outlined'
+}
+
+const formattedName = (name: string) => {
+  let res = ''
+  const segments = name.split('_')
+
+  for (let i = 0, l = segments.length; i < l; i++) {
+    const segment = segments[i]
+    res += `${ segment[0].toUpperCase() }${ segment.slice(1) }`
+  }
+
+  return res
+}
+
+const getDef = (): string => {
+  const _name = formattedName(name.value)
+  const _type = type.value
+
+  return icons[_type][`${ prefixesMap[_type] }${ _name }`]
+}
+
+type Paths = { 
   d: string, 
   style?: string, 
   transform?: string
-}[] = []
+}[]
 
-def.split('&&').map(path => {
-  const [ d, style, transform ] = path.split('@@')
-  segments.push({ style, d, transform })
+const genPaths = (
+  def: string, 
+  res: Paths
+) => {
+  def.split('&&').map(path => {
+    const [ d, style, transform ] = path.split('@@')
+    res.push({ style, d, transform })
+  })
+}
+
+const paths = computed(() => {
+  const res: Paths = []
+  const def = getDef()
+
+  genPaths(def, res)
+
+  return res
 })
 </script>
 
