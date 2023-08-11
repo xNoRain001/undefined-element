12b01bc75b8b38345a273047ac333586852e9aaa
xnorain001 <3385842328@qq.com>
2023-08-11 18:19:52 +0800

    update: dialog component

--- src/components/u-dialog/index.vue
+++ src/components/u-dialog/index.vue
@@ -1,35 +1,35 @@
 <template>
   <Teleport to="body">
-    <div 
-      v-show="_modelValue" 
-      class="u-dialog w-screen h-screen fixed top-0 left-0 z-50" 
-      @click="closeDialog"
-    >
+    <Transition name="u-animate-opacity" :duration="600000">
       <div 
-        ref="backdropRef"
-        class="u-dialog-backdrop h-full" 
-        :style="backgroundColorStyle"
+        v-if="modelValue" 
+        class="u-dialog w-screen h-screen fixed top-0 z-50" 
+        @click="closeDialog"
       >
-      </div>
-      <div 
-        ref="innerRef" 
-        class="u-dialog-inner fixed" 
-        :style="positionStyle"
-      >
-        <!-- this is a container for persistent -->
-        <div>
-          <slot></slot>
+        <div 
+          ref="backdropRef"
+          class="u-dialog-backdrop h-full" 
+          :class="backdropClass"
+        >
+        </div>
+        <div 
+          ref="innerRef" 
+          class="u-dialog-inner absolute flex" 
+          :class="innerClass"
+        >
+          <!-- this is a container for persistent -->
+          <div>
+            <slot></slot>
+          </div>
         </div>
       </div>
-    </div>
+    </Transition>
   </Teleport>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch, toRefs, computed } from 'vue'
+import { ref, toRefs, computed, watch } from 'vue'
 
-import { dialogPosition } from '../../const/strategies'
-import { genCSSVariables } from '../../utils'
 import { useAddAnimation } from '../../composables'
 
 const props = withDefaults(defineProps<{ 
@@ -44,7 +44,31 @@ const emit = defineEmits<{ 'update:modelValue': [value: boolean] }>()
 const { position, modelValue, persistent } = toRefs(props)
 const innerRef = ref<HTMLElement | null>(null)
 const backdropRef = ref<HTMLElement | null>()
-const _modelValue = ref(modelValue.value)
+const backdropClass = computed(() => modelValue.value 
+  ? 'bg-[rgba(0,0,0,.4)]' 
+  : 'bg-transparent'
+)
+const innerPositionClassStrategies = {
+  top: 'top-0 left-0 right-0 justify-center',
+  right: 'right-0 top-0 bottom-0 items-center',
+  bottom: 'bottom-0 left-0 right-0 justify-center',
+  left: 'left-0 top-0 bottom-0 items-center',
+  center: 'top-0 right-0 bottom-0 left-0 justify-center items-center'
+}
+const innerAnimationClassStrategies = {
+  top: ['animate-u-slide-down', 'animate-u-slide-up'],
+  right: ['animate-u-slide-left', 'animate-u-slide-right'],
+  bottom: ['animate-u-slide-up', 'animate-u-slide-down'],
+  left: ['animate-u-slide-right', 'animate-u-slide-left'],
+  center: []
+}
+const innerClass = computed(() => {
+  const _position = position.value
+
+  return `${ innerPositionClassStrategies[_position] } ${ 
+    innerAnimationClassStrategies[_position][modelValue.value ? '0' : '1'] 
+  }`
+})
 
 const closeDialog = (e: Event) => {
   const _inner = innerRef.value as HTMLElement
@@ -64,67 +88,11 @@ const closeDialog = (e: Event) => {
     // when dialog is opened and persistent, need to add animation, 
     // but don't add to inner node, because animation will clear 
     // inner node's transform property.
-    useAddAnimation(
-      _inner.children[0] as HTMLElement, 
-      'u-animate-dialog-inner-persistent', 
-      150
-    )
+    // useAddAnimation(
+    //   _inner.children[0] as HTMLElement, 
+    //   'u-animate-dialog-inner-persistent', 
+    //   150
+    // )
   }
 }
-
-const positionStyle = computed(() => {
-  const _position = position.value
-  const _modelValue = modelValue.value
-  const res: { [propName: string]: string } = {
-    ...dialogPosition[_position](),
-  }
-
-  if (_position === 'center') {
-    const { startValue, endValue } = genCSSVariables(_modelValue, '0', '1')
-
-    res.opacity = endValue
-    res[`--u-opacity-start`] = startValue,
-    res[`--u-opacity-end`] = endValue
-  } else {
-    const { startValue, endValue } = genCSSVariables(_modelValue, '-100%', '0')
-
-    res[_position] = endValue
-    res[`--u-${ _position }-start`] = startValue,
-    res[`--u-${ _position }-end`] = endValue
-  }
-
-  return res
-})
-
-const backgroundColorStyle = computed(() => {
-  const { startValue, endValue } = genCSSVariables(
-    modelValue.value, 
-    'transparent',
-    'rgba(0, 0, 0, .4)',
-  )
-
-  return {
-    backgroundColor: endValue,
-    '--u-bg-start': startValue,
-    '--u-bg-end': endValue
-  }
-})
-
-watch(modelValue, v => {
-  const _position = position.value
-
-  useAddAnimation(
-    innerRef.value as HTMLElement, 
-    `u-animate-${ _position }`
-  )
-  useAddAnimation(backdropRef.value as HTMLElement, 'u-animate-bg')
-
-  if (v) {
-    _modelValue.value = v
-  } else {
-    setTimeout(() => {
-      _modelValue.value = v
-    }, 300)
-  }
-})
 </script>
