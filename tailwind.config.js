0664d8f373864f503545e8bd9e623660d76fdee1
xnorain001 <3385842328@qq.com>
2023-08-11 18:19:16 +0800

    update: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{vue,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'u-slide-down': {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' }
+        },
+        'u-slide-up': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' }
+        },
+        'u-slide-right': {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' }
+        },
+        'u-slide-left': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' }
+        }
+      },
+      animation: {
+        'u-slide-down': 'u-slide-down var(--u-transition-duration)',
+        'u-slide-up': 'u-slide-up var(--u-transition-duration)',
+        'u-slide-right': 'u-slide-right var(--u-transition-duration)',
+        'u-slide-left': 'u-slide-left var(--u-transition-duration)'
+      }
+    },
   },
   plugins: [],
 }
