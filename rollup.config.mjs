8ac57a44a6abb9d660807c83bc1a9605b7baee52
xnorain001 <3385842328@qq.com>
2023-08-31 16:36:52 +0800

    update: rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,33 @@
 import vue from 'rollup-plugin-vue'
+import terser from '@rollup/plugin-terser'
 import typescript from 'rollup-plugin-typescript'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
-export default {
-  input: 'src/index.ts',
+const genConfig = (minify = false) => {
+  return ({
+    input: './src/index.ts',
 
-  output: {
-    file: './dist/index.js',
-    format: 'es'
-  },
+    output: {
+      file: `./dist/index${ minify ? '.min' : '' }.js`,
+      format: 'es'
+    },
 
-  external: ['vue'],
+    external: ['vue'],
 
-  plugins: [
-    vue(), 
-    typescript({
-      tsconfig: false,
-      experimentalDecorators: true,
-      module: 'es2015'
-    }),
-    peerDepsExternal()
-  ]
+    plugins: [
+      vue(), 
+      typescript({
+        tsconfig: false,
+        experimentalDecorators: true,
+        module: 'es2015'
+      }),
+      peerDepsExternal(),
+      minify ? terser() : null
+    ]
+  })
 }
+
+export default [
+  genConfig(),
+  genConfig(true)
+]
