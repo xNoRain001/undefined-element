c4144ef3451f735b5a8b68485246d7fda7f7a00a
xnorain001 <3385842328@qq.com>
2023-09-08 15:44:28 +0800

    update: package version

--- docs/postcss.config.mjs
+++ docs/postcss.config.mjs
@@ -1,5 +1,12 @@
-import { postcssIsolateStyles } from 'vitepress'
-
 export default {
-  plugins: [postcssIsolateStyles()]
+  plugins: {
+    'postcss-prefix-selector': {
+      prefix: ':not(:where(.vp-raw *))',
+      includeFiles: [/vp-doc\.css/],
+      transform(prefix, _selector) {
+        const [selector, pseudo = ''] = _selector.split(/(:\S*)$/)
+        return selector + prefix + pseudo
+      }
+    }
+  }
 }
